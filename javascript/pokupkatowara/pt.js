9c9015c6c59fec72475ce770e7af5f85630d0aa0
GirPlayer <temusahrik@gmail.com>
2024-12-12 18:04:32 +0300

    pokupka towara3

--- javascript/pokupkatowara/pt.js
+++ javascript/pokupkatowara/pt.js
@@ -12,19 +12,50 @@ let n4 = document.getElementById('n4')
 let n5 = document.getElementById('n5')
 
 function addTowar(){
-    n1.innerText=i1.value
-    n2.innerText=i2.value
-    n3.innerText=i3.value
-    n4.innerText=i4.value
-    n5.innerText=i5.value
+    n1.innerText = i1.value
+    n2.innerText = i2.value
+    n3.innerText = i3.value
+    n4.innerText = i4.value
+    n5.innerText = i5.value
 }
 b.addEventListener("click", addTowar)
 
+let towar = document.getElementById('towar');
 function delet(){
-    n1.innerText=""
-    n2.innerText=""
-    n3.innerText=""
-    n4.innerText=""
-    n5.innerText=""
+    elements.innerText = ""
+    elements.value = ""
 }
 b2.addEventListener("click", delet)
+
+let button = document.getElementById('b');
+let elements = document.getElementById('elements');
+let elementNumber = 0;
+
+function appendElement() {
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add('element');
+
+    const nameDiv = document.createElement('div');
+    nameDiv.innerHTML = `<span>Название товара: ${name.value}</span>`;
+    mainDiv.append(nameDiv)
+
+    const priceDiv = document.createElement('div');
+    priceDiv.innerHTML = `<span>Цена: ${price.value}</span>`;
+    mainDiv.append(priceDiv)
+
+    const colvoDiv = document.createElement('div');
+    colvoDiv.innerHTML = `<span>Количество товара: ${colvo.value}</span>`;
+    mainDiv.append(colvoDiv)
+
+    const artDiv = document.createElement('div');
+    artDiv.innerHTML = `<span>Артикул: ${art.value}</span>`;
+    mainDiv.append(artDiv)
+
+    const opisDiv = document.createElement('div');
+    opisDiv.innerHTML = `<span>Описание товара: ${opis.value}</span>`;
+    mainDiv.append(opisDiv)
+
+    elements.append(mainDiv)
+}
+
+b.addEventListener('click', appendElement)
