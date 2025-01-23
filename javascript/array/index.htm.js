9592ee2a48d054466504563a95c5c5155caf5e1a
GirPlayer <temusahrik@gmail.com>
2025-01-23 18:02:02 +0300

    пользователи :)

--- javascript/array/index.htm.js
+++ javascript/array/index.htm.js
@@ -1,3 +1,11 @@
+let avatarI = document.getElementsByClassName('avatarI')
+let nameI = document.getElementsByClassName('nameI')
+let phoneI = document.getElementsByClassName('phoneI')
+let emailI = document.getElementsByClassName('emailI')
+let birthdayI = document.getElementsByClassName('birthdayI')
+let b = document.getElementsByClassName('b')
+let deletI = document.getElementById('deletI')
+let b2 = document.getElementById('b2')
 //Массив в JS
 
 let users = ['Иван', 'Павел', 'Анна']
@@ -59,7 +67,7 @@ users = [
         email: 'anna@mail.ru',
         phone: '+7 (988) 111-100-11',
         birthday: '08.06.1996',
-    },
+    }
 ]
 
 //Получение значения объекта по его ключу
@@ -79,29 +87,69 @@ console.log(users)
 
 let usersList = document.getElementById('users-list')
 
+function deletUser(){
+    users.splice(deletI, 1)
+}
+b2.addEventListener('click', deletUser)
+
+function addUser(){
+    let user = {
+        avatar: avatarI[0].value,
+        name: nameI[0].value,
+        email: emailI[0].value,
+        phone: phoneI[0].value,
+        birthday: birthdayI[0].value,
+    }
+    users.push(user)
+    showUsers()
+}
+b[0].addEventListener('click', addUser)
 //Функция для вывода пользователей на экран
 function showUsers(){
-    //Создаем карточку пользователя
-    let userCard = document.createElement('div')
-    let userAvatar = document.createElement('div')
-    let userName = document.createElement('h2')
-    let userPhone = document.createElement('p')
-    let userEmail = document.createElement('p')
-    let userBirthday = document.createElement('p')
+    //Циклы в JS
+    //for (let элемент_массива of массив){
+    //  действия для выполнения элемента
+    //}
+    usersList.innerHTML = ''
+    for (let user of users){
 
-    //Добавляем карточке пользователя класс user-card
-    userCard.classList.add('user-card')
-    userAvatar.classList.add('user-avatar')
-    userName.classList.add('user-name')
 
-    userAvatar.innerHTML = `<img src="${firstUser.avatar}" alt="">`
-    userName.innerHTML = firstUser.name
-    userPhone.innerHTML = `<b>Телефон: </b>${firstUser.phone}`
-    userEmail.innerHTML = `<b>Email: </b>${firstUser.email}`
-    userBirthday.innerHTML = `<b>Дата рождение: </b>${firstUser.birthday}`
+        //Создаем карточку пользователя
+        let userCard = document.createElement('div')
+        let userAvatar = document.createElement('div')
+        let userName = document.createElement('h2')
+        let userPhone = document.createElement('p')
+        let userEmail = document.createElement('p')
+        let userBirthday = document.createElement('p')
 
-    userCard.append(userAvatar, userName, userPhone, userEmail, userBirthday)
+        //Добавляем карточке пользователя класс user-card
+        userCard.classList.add('user-card')
+        userAvatar.classList.add('user-avatar')
+        userName.classList.add('user-name')
 
-    usersList.append(userCard)
+        userAvatar.innerHTML = `<img src="${user.avatar}" alt="">`
+        userName.innerHTML = user.name
+        userPhone.innerHTML = `<b>Телефон: </b>${user.phone}`
+        userEmail.innerHTML = `<b>Email: </b>${user.email}`
+        userBirthday.innerHTML = `<b>Дата рождение: </b>${user.birthday}`
+
+        userCard.append(userAvatar, userName, userPhone, userEmail, userBirthday)
+
+        usersList.append(userCard)
+    }
 }
-showUsers();
+b.addEventListener('click', showUsers)
+
+// let numbers = [1, 2, 3, 4, 5, 6]
+// for (let num of numbers){
+//     console.log('Значение массива: ', num)
+//     num = num + 1
+// }
+//
+// numbers[0] = numbers[0] + 1
+// numbers[1] = numbers[1] + 1
+// numbers[2] = numbers[2] + 1
+// numbers[3] = numbers[3] + 1
+// numbers[4] = numbers[4] + 1
+// numbers[5] = numbers[5] + 1
+// console.log(numbers)
